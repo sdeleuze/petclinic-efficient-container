0aa1edf35121640f4365aea3848b5c595765c8bc
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-04-02 19:13:02 +0200

    Upgrade to Spring Boot 3

--- src/main/java/org/springframework/samples/petclinic/owner/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetController.java
@@ -19,7 +19,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.lang.Nullable;
